fe3f5fe7352408da8c9fc625e1ef19872522496e
Osmar Miranda <measurlinkbr@gmail.com>
2026-04-03 22:59:36 -0300

    feat: initialize WPF application with integrated Kestrel web host and project structure

--- ConnectML.UI/App.xaml.cs
+++ ConnectML.UI/App.xaml.cs
@@ -1,4 +1,10 @@
+using System;
+using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
 
 namespace ConnectML.UI
 {
@@ -7,5 +13,46 @@ namespace ConnectML.UI
     /// </summary>
     public partial class App : Application
     {
+        private IHost? _host;
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            _host = Host.CreateDefaultBuilder(e.Args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseKestrel(options =>
+                    {
+                        options.ListenAnyIP(5000);
+                    });
+                    
+                    webBuilder.Configure(app =>
+                    {
+                        app.UseRouting();
+                        app.UseEndpoints(endpoints =>
+                        {
+                            endpoints.MapGet("/", async context =>
+                            {
+                                await context.Response.WriteAsync("ConnectML Kestrel Host is running on WPF!");
+                            });
+                        });
+                    });
+                })
+                .Build();
+
+            await _host.StartAsync();
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            if (_host != null)
+            {
+                await _host.StopAsync(TimeSpan.FromSeconds(5));
+                _host.Dispose();
+            }
+
+            base.OnExit(e);
+        }
     }
 }
